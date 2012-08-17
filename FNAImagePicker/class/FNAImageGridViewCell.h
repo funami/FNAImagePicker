53391479b62845155dbca5542a6015251a077d1b
iRSS <takao.funami@gmail.com>
2012-08-17 20:10:17 +0900

    AQGridを使うように改修中

---
+++ FNAImagePicker/class/FNAImageGridViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FNAImageGridViewCell.h
+//  FNAImagePicker
+//
+//  Created by funami on 2012/08/16.
+//  Copyright (c) 2012年 funami. All rights reserved.
+//
+
+#import "AQGridViewCell.h"
+
+@interface FNAImageGridViewCell : AQGridViewCell
+
+@end
---
+++ FNAImagePicker/class/FNAImageGridViewCell.m
@@ -0,0 +1,31 @@
+//
+//  FNAImageGridViewCell.m
+//  FNAImagePicker
+//
+//  Created by funami on 2012/08/16.
+//  Copyright (c) 2012年 funami. All rights reserved.
+//
+
+#import "FNAImageGridViewCell.h"
+
+@implementation FNAImageGridViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
